f598742893705bd1bf616369627a7fa040a95853
alvas <alvations@gmail.com>
2014-08-18 10:57:03 +0200

    in a mess...

---
+++ ntumc/toolkit/kor.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import os, io, subprocess, time
+import sys; reload(sys); sys.setdefaultencoding('utf-8')
+
+class Postech():
+    def __init__(self, sejong_dir= '/home/alvas/git/NTU-MC/ntumc/external/sejong/'):
+        self.sejong_dir = sejong_dir
+    
+    def utf8_to_euck(self, text):
+        text = unicode(text.decode('utf-8'))
+        text = text.replace(u'\xa0', u' ')
+        text = text.replace(u'\xe7', u'c') # ç -> c
+        text = text.replace(u'\xe9', u'e') # é -> e
+        text = text.replace(u'\u2013', u'-') # – -> -
+        text = text.replace(u'\xa9', '(c)') # © -> (c)
+        return text.encode('euc-kr').strip()
+    
+    def sejong(self, text):
+        text = utf8_to_euck(text)
+        sejong_dir = self.sejong_dir
+        with io.open(sejong_dir+'input.txt', 'wb') as fout:
+            fout.write(text)
+        
+        cmd = "".join(['wine start /Unix ', sejong_dir,'sjTaggerInteg.exe'])
+        os.popen(cmd)
+        time.sleep(2)
+        
+        with io.open(sejong_dir+'output.txt', 'r', encoding='euc-kr') as fin:
+            sejongtext = fin.read().strip().encode('utf8').decode('utf8')
+        
+        return sejongtext
+    
+    def tag(self, text):
+        sejong(text)
+        tagged_text = [tuple(i.split(r'/')) for i in sejongtext.split()]
+        return tagged_text
+    
+    def tokenize(self, text):
+        sejong(text)
+        text = " ".join([i.split(r'/')[0] for i in sejongtext.split()])
+        return text
+    
+    
---
+++ ntumc/toolkit/vie.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os, io, subprocess
+import sys; reload(sys); sys.setdefaultencoding('utf-8')
+
+def Jvntextpro():
+    def __init__(self, jvn_dir='/home/alvas/JVnTextPro-v.2.0/'):
+        self.jvn_dir = jvn_dir
+    
+        self.segmenter_cmd = "".join(['java -mx512M -cp ',
+                                       jvn_dir, '/bin:', 
+                                       jvn_dir + '/libs/args4j.jar:',
+                                       jvn_dir + '/libs/lbfgs.jar ',
+                                       'jvnsegmenter.WordSegmenting ',
+                                       '-modeldir ', jvn_dir, 
+                                       '/models/jvnsegmenter ',
+                                       '-inputfile tmp.txt',
+                                       ' -outputfile tmp.txt.wseg'])
+        
+    def tokenize(self, text):
+        print text
+        # Write to text to temp file.
+        os.popen("".join(['echo "', text, '" > tmp.txt']))
+        # Runs segmenter.
+        os.popen(self.segmenter_cmd)
+        # Reads from output file.
+        text = io.open('tmp.txt.wseg', 'r', encoding='utf8').read().strip()
+        return text
